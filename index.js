8e724c0c268a7011a78919fce1367b759bb617ea
ibanb <ibonburger@mail.ru>
2022-04-11 11:25:18 +0300

    setTimeout recursive

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
-
 function printNumber(from, to) {
 
-  const timerId = setInterval(() => {
+  const delay = 1000;
+  const timerId = setTimeout(function run() {
     from = from + 1;
+    console.log(from);
+
     if (from === to) {
-      clearInterval(timerId);
+     clearTimeout(timerId);
     } else {
-      console.log(from);
+     setTimeout(run, delay);
     }
-  }, 1000);
+  }, delay);
 }
 
 printNumber(3, 10);
