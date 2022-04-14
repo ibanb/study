c6901088c2be3fdaf169e36b8c0b3ace288434b8
ibanb <ibonburger@mail.ru>
2022-04-14 14:36:40 +0300

    Анаграмма 2 short

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-// анаграмма
+// анаграмма 2
 
 const anagram = (data) => {
 
-  const frame = data[0].split('')
-  let buffer = [...frame]
-  let result = true
-
-  data.slice(1).map(item => {
-    if (buffer.length !== item.length) {
-      result = false
-    }
-
-    for (let i = 0; i < item.length; i++) {
-      if (buffer.includes(item[i])) {
-        const index = buffer.indexOf(item[i])
-        buffer.splice(index, 1)
-      }
-    }
-    if (buffer.length !== 0) {
-      result = false
-    }
-    buffer = [...frame]
-  })
-  return result
+  return data.map(item => item.split('').sort().join(''))
+  .filter(item => item !== data[0]).length === 0
 }
-
-console.log(anagram(['abcde', 'acdb', 'adbc']))
 
