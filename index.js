4c6dd53890443d254348d6057642efe84e3b0c83
ibanb <ibonburger@mail.ru>
2022-04-14 14:22:28 +0300

    Анаграмма

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+// анаграмма
 
-// Подмножество массива
+const anagram = (data) => {
 
-const contains = (where, what) => {
-
+  const frame = data[0].split('')
+  let buffer = [...frame]
   let result = true
 
-  what.map(item => {
-    if (!where.includes(item)) {
+  data.slice(1).map(item => {
+    if (buffer.length !== item.length) {
       result = false
     }
-  })
 
+    for (let i = 0; i < item.length; i++) {
+      if (buffer.includes(item[i])) {
+        const index = buffer.indexOf(item[i])
+        buffer.splice(index, 1)
+      }
+    }
+    if (buffer.length !== 0) {
+      result = false
+    }
+    buffer = [...frame]
+  })
   return result
 }
 
-console.log(contains([1, 2, 3, 4], [1, 2, 5]))
-
+console.log(anagram(['abcde', 'acdb', 'adbc']))
 
